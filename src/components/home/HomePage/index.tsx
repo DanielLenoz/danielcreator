6af66d689bf09e7b9c38331aef224071d660557f
DanielLenoz <danielstivenxx@outlook.com>
2024-04-13 14:50:38 -0500

    hotfix bottom

--- src/components/home/HomePage/index.tsx
+++ src/components/home/HomePage/index.tsx
@@ -18,8 +18,8 @@ export const HomePage = () => {
         width={360}
         height={360}
       />
-      <section className="bg-mygradiente z-10 grid min-h-[400px] max-w-[604px] justify-items-start gap-2 justify-self-end p-2">
-        <p className="bg-mygradiente rounded-lg p-1 font-titleSubtitle text-4xl font-bold">
+      <section className="z-10 grid min-h-[400px] max-w-[604px] justify-items-start gap-2 justify-self-end bg-mygradiente p-2">
+        <p className="rounded-lg bg-mygradiente p-1 font-titleSubtitle text-4xl font-bold">
           Frontend Developer / Designer UI/UX
         </p>
         <p className=" font-titleSubtitle text-5xl font-extrabold">
@@ -30,7 +30,7 @@ export const HomePage = () => {
         </p>
         <button>
           <Link
-            className=" inline-block rounded-lg bg-segundaryDark px-2 py-1  font-textPrimary text-base font-medium text-white"
+            className="hover:bg-tertiary inline-block rounded-lg bg-segundaryDark px-2 py-1  font-textPrimary text-base font-medium text-white"
             href={'/contacto'}
           >
             Contacto
